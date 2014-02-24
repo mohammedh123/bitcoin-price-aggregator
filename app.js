d02b023c3f67cc7592db5c7876d7f25167091a03
Mohammed Hossain <mohammedh123@gmail.com>
2014-02-24 17:53:55 -0500

    Updates the routes a bit.

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ var mostRecentTrades = [];
 
 var express = require('express');
 var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 var util = require('util');
