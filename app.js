438f88dd0e157220c27b9f95b833847a09739fc5
Mohammed Hossain <mohammedh123@gmail.com>
2014-02-24 17:22:42 -0500

    Adds support for Bitstamp trades and replaces use of console.log with util.log.

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var util = require('util');
 var io = require('socket.io-client');
 
 var app = express();
@@ -41,6 +42,55 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+// maybe use a js object instead
+function AddTrade(timestamp, price, quantity, exchange, id)
+{
+	var newTrade = {};
+	
+	newTrade.timestamp = timestamp;
+	newTrade.price = price;
+	newTrade.quantity = quantity;
+	newTrade.exchange = exchange;
+	newTrade.id = id;
+	
+	mostRecentTrades.push(newTrade);
+}
+
+function UpdateTrades() {	
+	//Bitstamp trade population
+	util.log("Updating trades from Bitstamp...");
+	
+	 //only get the last minute worth of transactions
+	var btOptions = {
+		host: 'www.bitstamp.net',
+		port: 80,
+		path: '/api/transactions/?time=minute'
+	};
+	
+	http.get(btOptions, function(res) {
+		if(res.statusCode == 200) {
+			var body = '';
+			res.on('data', function(chunk) {
+				body += chunk;
+			});
+			res.on('end', function() {			
+				var trades = JSON.parse(body);
+				for(var i = 0; i < trades.length; ++i) {
+				// we now have the trade info in a json object, add it to the list
+					var currTrade = trades[i];
+					
+					AddTrade(currTrade.date, currTrade.price*100000000, currTrade.amount*100000000, "Bitstamp", currTrade.tid);
+				}
+			});
+		}
+	}).on('error', function(e) {
+		util.log("Got error: " + e.message);
+	});
+}
+
+// periodically update the internal trade list (for Bitstamp)
+setInterval(UpdateTrades, 10000);
+
 app.get('/', routes.index);
 app.get('/TradeData', function(req, res){
 	var tradeData = {};
@@ -51,12 +101,12 @@ app.get('/TradeData', function(req, res){
 
 var server = http.createServer(app);
 server.listen(app.get('port'), function(){
-	console.log('Express server listening on port ' + app.get('port'));
+	util.log('Express server listening on port ' + app.get('port'));
 
 	// MtGox trade population
 	var socket = io.connect('socketio.mtgox.com/mtgox?Currency=USD');
 	socket.on('connect', function() {
-		console.log("Connected to MtGox websocket.");
+		util.log("Connected to MtGox websocket.");
 		var msg = {};
 		msg.op = "mtgox.subscribe";
 		msg.type = "trades";
@@ -75,6 +125,6 @@ server.listen(app.get('port'), function(){
 		}
 	});
 	socket.on('disconnect', function() {
-		console.log("Disconnected from MtGox websocket.");
+		util.log("Disconnected from MtGox websocket.");
 	});
 });
